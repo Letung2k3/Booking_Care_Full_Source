2aab586565631205441c35aa2e18fc8757eef7fd
Le Thanh Tung <letung767768@gmail.com>
2024-11-21 17:29:09 +0700

    Source code Booking Care

---
+++ src/migrations/migration-create-extraInfo.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('ExtraInfos', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            patientId: {
+                type: Sequelize.INTEGER
+            },
+            historyBreath: {
+                type: Sequelize.TEXT
+            },
+            placeId: {
+                type: Sequelize.INTEGER,
+                allowNull: true,
+            },
+            oldForms: {
+                type: Sequelize.TEXT
+            },
+            sendForms: {
+                type: Sequelize.TEXT
+            },
+            moreInfo: {
+                type: Sequelize.TEXT
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: true,
+                type: Sequelize.DATE
+            },
+            deletedAt: {
+                allowNull: true,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('ExtraInfos');
+    }
+};
